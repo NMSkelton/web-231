783f4c5c91569f699bed09f1dfd3a330a7b6b3c3
Nicholas Skelton <nicholasskelton30@gmail.com>
2025-08-21 00:37:02 -0500

    Week 2 Push

---
+++ week-2/project02-02/project02-02.js
@@ -0,0 +1,20 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: Nicholas Skelton
+      Date: 8.20.25
+
+      Filename: project02-02.js
+ */
+
+function verifyForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+
+  (name) && (email) && (phone) ? window.alert("Thank you!") : window.alert("Please fill in all fields");
+}
+
+document.getElementById("submit").addEventListener("click", verifyForm);
