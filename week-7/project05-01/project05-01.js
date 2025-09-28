fa770d15cd49ab4ea020e45d2e2f19b838800282
Nicholas Skelton <nicholasskelton30@gmail.com>
2025-09-27 23:59:39 -0500

    Week 7 Push

---
+++ week-7/project05-01/project05-01.js
@@ -0,0 +1,70 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 5
+      Project 05-01
+
+      Project to present an online quiz with a countdown clock
+      Author: Nicholas Skelton
+      Date: 9.27.25
+
+      Filename: project05-01.js
+*/
+
+// Constants to set the time given for the quiz in seconds
+// and the correct answers to each quiz question
+const quizTime = 90;
+const correctAnswers = ["10", "4", "-6", "5", "-7"];
+
+let timeID;
+let questionList = document.querySelectorAll("div#quiz input");
+
+// Elements in the quiz page
+let startQuiz = document.getElementById("startquiz");
+let quizClock = document.getElementById("quizclock");
+let overlay = document.getElementById("overlay");
+
+// Initialize the quiz time
+quizClock.value = quizTime;
+let timeLeft = quizTime;
+
+// Declare the ID for timed commands
+// and the node list for questions
+
+startQuiz.onclick = function () {
+  overlay.className = "showquiz";
+  timeID = window.setInterval(countdown, 1000);
+}
+
+function countdown () {
+  if (timeLeft === 0) {
+    clearInterval(timeID);
+    let totalCorrect = checkAnswers();
+    if (totalCorrect === correctAnswers.length) {
+      window.alert("Great job!");
+    } else {
+      window.alert("You missed " + (questionList.length - totalCorrect) + " out of " + questionList.length + ".");
+      timeLeft = quizTime;
+      quizClock.value = timeLeft;
+      overlay.className = "hidequiz";
+    }
+  } else {
+    timeLeft--;
+    quizClock.value = timeLeft;
+  }
+}
+
+/*------------- Function to check the student answers ----------------*/
+function checkAnswers() {
+   let correctCount = 0;
+
+   for (let i = 0; i < questionList.length; i++) {
+      if (questionList[i].value === correctAnswers[i]) {
+         correctCount++;
+         questionList[i].className = "";
+      } else {
+         questionList[i].className = "wronganswer";
+      }
+   }
+   return correctCount;
+}
+
